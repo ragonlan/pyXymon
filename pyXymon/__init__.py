03a801aaf6f0e15af3bd9f66eeb70d245ce0003b
Raul Gonzalez <raul.gonzalez@acens.com>
2016-11-10 19:06:34 +0100

    Follow style python programing and remove some debug lines.

--- pyXymon/__init__.py
+++ pyXymon/__init__.py
@@ -5,7 +5,8 @@ import re
 from datetime import datetime, date, time
 from subprocess import Popen, PIPE
 import pprint
-import logging, sys
+import logging
+import sys
 FORMAT = '%(asctime)-15s %(name)s [%(process)d] %(levelname)s: %(message)s'
 #logging.basicConfig(stream=sys.stderr, level=logging.INFO, format=FORMAT)
 pp = pprint.PrettyPrinter(indent=2)
@@ -13,18 +14,21 @@ pp = pprint.PrettyPrinter(indent=2)
 __version__ = '0.0.1'
 
 color = {
-        'blue'  : 5,
-        'clear' : 0,
-        'green' : 1,
-        'purple': 2,
-        'yellow': 3,
-        'red'   : 4,
-        }
+    'blue': 5,
+    'clear': 0,
+    'green': 1,
+    'purple': 2,
+    'yellow': 3,
+    'red': 4,
+}
 
-validDataType = ('GAUGE', 'COUNTER', 'DERIVE', 'DCOUNTER', 'DDERIVE', 'ABSOLUTE')
+validDataType = ('GAUGE', 'COUNTER', 'DERIVE',
+                 'DCOUNTER', 'DDERIVE', 'ABSOLUTE')
+
 
 class Xymon(object):
     """docstring for ."""
+
     def __init__(self, type='status', server=None, port=1984, host=None, color='clear', test='test', title=None, text='', lifetime='30', debug=False):
         """
            type : type of message sended to display. default "status"
@@ -37,29 +41,32 @@ class Xymon(object):
            debug : debug
          """
         if debug is False:
-            logging.basicConfig(stream=sys.stderr, level=logging.INFO, format=FORMAT)
+            logging.basicConfig(stream=sys.stderr,
+                                level=logging.INFO, format=FORMAT)
         else:
-            logging.basicConfig(stream=sys.stderr, level=logging.DEBUG, format=FORMAT)
+            logging.basicConfig(stream=sys.stderr,
+                                level=logging.DEBUG, format=FORMAT)
 
         self.cfgvar = Xymon.LoadConf()
         if server is None:
             server = cfgvar['XYMSRV']
         if host is None:
             host = cfgvar['CLIENTHOSTNAME']
-        self.type     = type
-        self.server   = server
-        self.port     = port
-        self.host     = host
-        self.color    = color
-        self.test     = test
-        self.title    = title
-        self.text     = text
+        self.type = type
+        self.server = server
+        self.port = port
+        self.host = host
+        self.color = color
+        self.test = test
+        self.title = title
+        self.text = text
         self.lifetime = lifetime
 
     @classmethod
     def guessCfgFile(self):
         """guess witch is the configuration file"""
-        cfgfiles = ('/etc/xymon/xymonclient.cfg', '/etc/xymon/xymonserver.cfg','/etc/hobbit/xymonclient.cfg','/etc/hobbit/hobbitclient.cfg')
+        cfgfiles = ('/etc/xymon/xymonclient.cfg', '/etc/xymon/xymonserver.cfg',
+                    '/etc/hobbit/xymonclient.cfg', '/etc/hobbit/hobbitclient.cfg')
         for f in cfgfiles:
             if os.path.exists(f):
                 return f
@@ -70,7 +77,8 @@ class Xymon(object):
         if file is None:
             file = Xymon.guessCfgFile()
             logging.debug('configfile detected: {0}'.format(file))
-        varpattern = re.compile('\$(\w+)(.*)')  # Only varible occurrence at the begining
+        # Only varible occurrence at the begining
+        varpattern = re.compile('\$(\w+)(.*)')
 
         for line in open(file, 'r').readlines():
             li = line.strip().partition('#')[0]
@@ -80,14 +88,17 @@ class Xymon(object):
                     varname = parts[0]
                     content = parts[1].strip("\" \t")
                     try:
-                        vars[varname] = re.sub(r'\$(\w+)\b', lambda m:(vars[m.group(1)]), content)
-                        logging.debug("{0} {1} <<<{2}>>>".format(file, varname, vars[varname]))
+                        vars[varname] = re.sub(
+                            r'\$(\w+)\b', lambda m: (vars[m.group(1)]), content)
+                        logging.debug("{0} {1} <<<{2}>>>".format(
+                            file, varname, vars[varname]))
                     except KeyError:
-                        pass  #inner variable reference is not defined previusly.
+                        # inner variable reference is not defined previusly.
+                        pass
                 elif parts[0].startswith('include'):
                     parts[0] = parts[0].strip('include ')
                     logging.debug(">>>>{0}".format(parts[0]))
-                    vars.update(self.LoadConf( parts[0], vars ))
+                    vars.update(self.LoadConf(parts[0], vars))
                     logging.debug("<<<<{0}".format(parts[0]))
         # Second pass to substitue variables referenced
         return vars
@@ -100,6 +111,7 @@ class Xymon(object):
             return color2
         else:
             return color1
+
     def clear(self):
         """set color to clear if is more critical than current color"""
         return self.addColor('cleara')
@@ -115,31 +127,32 @@ class Xymon(object):
     def red(self):
         """set color to red if is more critical than current color"""
         return self.addColor('red')
+
     def addColor(self, color):
         """set colro to 'color' if is more critical than current color"""
         self.color = self.maxColor(self.color, color)
 
     def colorPrint(self, color, text):
         """add text to message and set 'color' as current color if is more critical"""
-        self.text += text;
+        self.text += text
         return self.addColor(color)
 
     def colorLine(self, color, text):
         """add color gif in the same line than text and set 'color' as current color if is more critical"""
-        self.text += '&' + color + ' ' + text;
+        self.text += '&' + color + ' ' + text
         return self.addColor(color)
 
     def printLine(self, text):
         """add text to message"""
-        self.text += text;
+        self.text += text
 
     def say(self, text):
         """add text to message witch carrier return"""
-        self.text += text + "\n";
+        self.text += text + "\n"
 
     def color(self):
         """return current color"""
-        #print(self.color)
+        # print(self.color)
         return(self.color)
 
     def send(self):
@@ -151,20 +164,27 @@ class Xymon(object):
                 self.title = 'test OK'
             else:
                 self.title = 'test NOT ok'
-        self.title = datef +' - '+ self.title
+        self.title = datef + ' - ' + self.title
         if self.type == 'status':
-            report = '{0}+{1} {2}.{3} {4} {5} \n {6}'.format(self.type , self.lifetime , self.host , self.test, self.color, self.title, self.text)
+            report = '{0}+{1} {2}.{3} {4} {5} \n {6}'.format(
+                self.type, self.lifetime, self.host, self.test, self.color, self.title, self.text)
         else:
-            report = '{0} {1}.{2} {3} \n {4}'.format(self.type , self.host , self.test, self.title, self.text)
+            report = '{0} {1}.{2} {3} \n {4}'.format(
+                self.type, self.host, self.test, self.title, self.text)
 
         if os.environ.get('XYMON'):
-            p = Popen([self.cfgvar['BB'], self.cfgvar['BBDISP'], '@'], stdout=PIPE, stdin=PIPE)
+            p = Popen([self.cfgvar['BB'], self.cfgvar[
+                      'BBDISP'], '@'], stdout=PIPE, stdin=PIPE)
             p.communicate(report)
-            logging.debug('{0} {1} {2}'.format(self.cfgvar['BB'], self.cfgvar['BBDISP'], report))
+            logging.debug('{0} {1} {2}'.format(
+                self.cfgvar['BB'], self.cfgvar['BBDISP'], report))
         else:
-            logging.info('{0} {1} {2}'.format(self.cfgvar['BB'], self.cfgvar['BBDISP'], report))
+            logging.info('{0} {1} {2}'.format(
+                self.cfgvar['BB'], self.cfgvar['BBDISP'], report))
+
 
 class XymonGraph(Xymon):
+
     def __init__(self, server=None, port=1984, host=None, test='test', rrdname=None, datatype='GAUGE', min='0', max='U', heartbeat=600):
         """
         server : hostname or IP message is sended to. Display server.
@@ -183,16 +203,16 @@ class XymonGraph(Xymon):
         if host is None:
             host = cfgvar['CLIENTHOSTNAME']
 
-        self.server   = server
-        self.port     = port
-        self.host     = host
-        self.test     = test.replace('/','_')
+        self.server = server
+        self.port = port
+        self.host = host
+        self.test = test.replace('/', '_')
         self.datatype = datatype
-        self.min      = min
-        self.max      = max
+        self.min = min
+        self.max = max
         self.heartbeat = heartbeat
         if rrdname is not None:
-            self.rrdname = rrdname.replace('/','_')
+            self.rrdname = rrdname.replace('/', '_')
         else:
             self.rrdname = None
         if self.datatype not in validDataType:
@@ -201,10 +221,12 @@ class XymonGraph(Xymon):
 
     def insert(self, dataval, dataname='ds0', datatype='GAUGE'):
         if datatype not in validDataType:
-            raise ValueError('invalid datatype parameter "{0}"'.format(datatype))
+            raise ValueError(
+                'invalid datatype parameter "{0}"'.format(datatype))
         self.data[self.rrdname] = dict()
         self.data[self.rrdname][dataname] = [datatype, dataval]
-        logging.debug('rrdname={0}:dataname={1} dataval={2} datatype={3}'.format(self.datatype, dataname, dataval, datatype))
+        logging.debug('rrdname={0}:dataname={1} dataval={2} datatype={3}'.format(
+            self.datatype, dataname, dataval, datatype))
 
     def show(self):
         output = 'data {0}.trends\n'.format(self.host)
@@ -215,16 +237,20 @@ class XymonGraph(Xymon):
 
         for rrdname, dataDict in self.data.iteritems():
             for dataname, dataArray in dataDict.iteritems():
-                output += 'DS:{0}:{1}:{2}:{3}:{4}\t{5}\n'.format(dataname, dataArray[0], self.heartbeat, self.min, self.max,  dataArray[1] )
+                output += 'DS:{0}:{1}:{2}:{3}:{4}\t{5}\n'.format(
+                    dataname, dataArray[0], self.heartbeat, self.min, self.max,  dataArray[1])
         logging.debug(output)
-        #print output
+        # print output
         return output
 
     def send(self):
         report = self.show()
         if os.environ.get('XYMON'):
-            p = Popen([self.cfgvar['BB'], self.cfgvar['BBDISP'], '@'], stdout=PIPE, stdin=PIPE)
+            p = Popen([self.cfgvar['BB'], self.cfgvar[
+                      'BBDISP'], '@'], stdout=PIPE, stdin=PIPE)
             p.communicate(report.encode('utf8'))
-            logging.debug('{0} {1} {2}'.format(self.cfgvar['BB'], self.cfgvar['BBDISP'], report.encode('utf8')))
+            logging.debug('{0} {1} {2}'.format(
+                self.cfgvar['BB'], self.cfgvar['BBDISP'], report.encode('utf8')))
         else:
-            logging.info('{0} {1} {2}'.format(self.cfgvar['BB'], self.cfgvar['BBDISP'], report.encode('utf8')))
+            logging.info('{0} {1} {2}'.format(
+                self.cfgvar['BB'], self.cfgvar['BBDISP'], report.encode('utf8')))
--- tests/xymontest.py
+++ tests/xymontest.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from pyXymon import Xymon,XymonGraph
+from pyXymon import Xymon, XymonGraph
 import sys
 
-x = Xymon(test='test', server='xymon1.acens.priv', host='pru.server.priv', debug=False)
-vars = x.LoadConf('/etc/xymon/xymonclient.cfg');
+x = Xymon(test='test', server='xymon1.acens.priv',
+          host='pru.server.priv', debug=False)
 x.green()
 x.say("test text")
-x.colorLine('yellow',"Xymon client home is "+vars['XYMONCLIENTHOME'])
+#vars = x.LoadConf('/etc/xymon/xymonclient.cfg');
+x.colorLine('yellow', "Xymon client home is " + x.cfgvar['XYMONCLIENTHOME'])
 x.red()
 x.send()
 
 
-xg=XymonGraph(test='test', server='xymon1.server.priv', host='pru.server.priv')
+xg = XymonGraph(test='test', server='xymon1.server.priv',
+                host='pru.server.priv', rrdname='/dev/sda1')
 xg.insert(51, 'ds0')
-#xg.show()
+out = xg.show()
+print(">>" + out + "<<")
 xg.send()
