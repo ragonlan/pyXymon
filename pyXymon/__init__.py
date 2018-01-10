7a8ea6e99db0b19663855c99c75db53e9eb08158
Raul Gonzalez <ragonlan@gmail.com>
2018-01-10 15:58:38 +0100

    Fix config files order so it work in xymon servers

--- pyXymon/__init__.py
+++ pyXymon/__init__.py
@@ -69,8 +69,9 @@ class Xymon(object):
     @classmethod
     def guessCfgFile(self):
         """guess witch is the configuration file"""
-        cfgfiles = ('/etc/xymon/xymonclient.cfg', '/etc/xymon/xymonserver.cfg',
-                    '/etc/hobbit/xymonclient.cfg', '/etc/hobbit/hobbitclient.cfg', '/etc/xymon/hobbitclient.cfg', '/etc/xymon-client/xymonclient.cfg')
+        cfgfiles = ('/etc/xymon/xymonserver.cfg', '/etc/xymon/xymonclient.cfg',
+                    '/etc/hobbit/xymonclient.cfg', '/etc/hobbit/hobbitclient.cfg',
+                    '/etc/xymon/hobbitclient.cfg', '/etc/xymon-client/xymonclient.cfg')
         for f in cfgfiles:
             if os.path.exists(f):
                 return f
